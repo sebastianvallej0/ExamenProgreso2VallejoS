bd2b8e502edf702d432d83c060af37ac4cc56400
sebastianvallej0 <vallejosebastian90@gmail.com>
2025-06-03 10:11:31 -0500

    Correccion de Abaout.page y chistes.xaml

--- ExamenProgreso2VallejoS/ChistesPage.xaml.cs
+++ ExamenProgreso2VallejoS/ChistesPage.xaml.cs
@@ -1,10 +1,41 @@
-namespace ExamenProgreso2VallejoS;
+using Newtonsoft.Json;
 
+namespace ExamenProgreso2VallejoS;
 
 public partial class ChistesPage : ContentPage
 {
-	public ChistesPage()
-	{
-		InitializeComponent();
-	}
+    public ChistesPage()
+    {
+        InitializeComponent();
+        ObtenerChiste(); // Al cargar la página
+    }
+
+    private async void ObtenerChiste()
+    {
+        try
+        {
+            using HttpClient client = new HttpClient();
+            string url = "https://official-joke-api.appspot.com/random_joke";
+            var response = await client.GetStringAsync(url);
+
+            var chiste = JsonConvert.DeserializeObject<Chiste>(response);
+            ChisteLabel.Text = $"{chiste.Setup}\n\n{chiste.Punchline}";
+        }
+        catch (Exception ex)
+        {
+            ChisteLabel.Text = "No se pudo cargar el chiste. Verifica tu conexión.";
+        }
+    }
+
+    private void OnOtroChisteClicked(object sender, EventArgs e)
+    {
+        ObtenerChiste();
+    }
+
+    public class Chiste
+    {
+        public string Type { get; set; }
+        public string Setup { get; set; }
+        public string Punchline { get; set; }
+    }
 }
